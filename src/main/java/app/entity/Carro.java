4c0f1a340211dc9cb6cb1273037b5be010c9056a
Saler C. Lourenço <saler.cez@gmail.com>
2024-02-21 20:18:22 -0300

    refactor: listaCarros as private attribute in service class

--- src/main/java/app/entity/Carro.java
+++ src/main/java/app/entity/Carro.java
@@ -2,11 +2,13 @@ package app.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Carro {
 	private String nome;
 	private String marca;
--- src/main/java/app/service/CarroService.java
+++ src/main/java/app/service/CarroService.java
@@ -10,63 +10,59 @@ import app.entity.Carro;
 @Service
 public class CarroService {
 	
+	private List<Carro> listaCarros;
+	
+	public CarroService() {
+		Carro carroAleatorio = new Carro();
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Voyage");
+		carroAleatorio.setMarca("Volkswagen");
+		carroAleatorio.setModelo("Sedan");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Civic");
+		carroAleatorio.setMarca("Honda");
+		carroAleatorio.setModelo("Sedan");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Corolla");
+		carroAleatorio.setMarca("Toyota");
+		carroAleatorio.setModelo("Sedan");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Golf");
+		carroAleatorio.setMarca("Volkswagen");
+		carroAleatorio.setModelo("Hatch");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Focus");
+		carroAleatorio.setMarca("Ford");
+		carroAleatorio.setModelo("Hatch");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Fusion");
+		carroAleatorio.setMarca("Ford");
+		carroAleatorio.setModelo("Sedan");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Cruze");
+		carroAleatorio.setMarca("Chevrolet");
+		carroAleatorio.setModelo("Sedan");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Onix");
+		carroAleatorio.setMarca("Chevrolet");
+		carroAleatorio.setModelo("Hatch");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("HB20");
+		carroAleatorio.setMarca("Hyundai");
+		carroAleatorio.setModelo("Hatch");
+		this.listaCarros.add(carroAleatorio);
+		carroAleatorio.setNome("Creta");
+		carroAleatorio.setMarca("Hyundai");
+		carroAleatorio.setModelo("SUV");
+		this.listaCarros.add(carroAleatorio);
+	}
+	
 	public String save(Carro carro) {
 		return carro.getNome() + " foi salvo com sucesso!";
 	}
 	
 	public List<Carro> listAll(){
-		List<Carro> listaCarro = new ArrayList<Carro>();
-		Carro carroAleatorio = new Carro("Palio", "Fiat", "Hatch");
-		listaCarro.add(carroAleatorio);
-		carroAleatorio.setNome("Voyage");
-		carroAleatorio.setMarca("Volkswagen");
-		carroAleatorio.setModelo("Sedan");
-		listaCarro.add(carroAleatorio);
-		carroAleatorio.setNome("Civic");
-		carroAleatorio.setMarca("Honda");
-		carroAleatorio.setModelo("Sedan");
-		listaCarro.add(carroAleatorio);
-
-		carroAleatorio.setNome("Corolla");
-		carroAleatorio.setMarca("Toyota");
-		carroAleatorio.setModelo("Sedan");
-		listaCarro.add(carroAleatorio);
-
-		carroAleatorio.setNome("Golf");
-		carroAleatorio.setMarca("Volkswagen");
-		carroAleatorio.setModelo("Hatch");
-		listaCarro.add(carroAleatorio);
-
-		carroAleatorio.setNome("Focus");
-		carroAleatorio.setMarca("Ford");
-		carroAleatorio.setModelo("Hatch");
-		listaCarro.add(carroAleatorio);
-
-		carroAleatorio.setNome("Fusion");
-		carroAleatorio.setMarca("Ford");
-		carroAleatorio.setModelo("Sedan");
-		listaCarro.add(carroAleatorio);
-
-		carroAleatorio.setNome("Cruze");
-		carroAleatorio.setMarca("Chevrolet");
-		carroAleatorio.setModelo("Sedan");
-		listaCarro.add(carroAleatorio);
-
-		carroAleatorio.setNome("Onix");
-		carroAleatorio.setMarca("Chevrolet");
-		carroAleatorio.setModelo("Hatch");
-		listaCarro.add(carroAleatorio);
-
-		carroAleatorio.setNome("HB20");
-		carroAleatorio.setMarca("Hyundai");
-		carroAleatorio.setModelo("Hatch");
-		listaCarro.add(carroAleatorio);
-
-		carroAleatorio.setNome("Creta");
-		carroAleatorio.setMarca("Hyundai");
-		carroAleatorio.setModelo("SUV");
-		listaCarro.add(carroAleatorio);
 		
-		return listaCarro;
+		return this.listaCarros;
 	}
 }
